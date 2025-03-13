80a694d20acb6af19a741dcd4678437984237691
jtclemm <jtclemm@sandia.gov>
2025-03-13 14:11:10 -0600

    Prototyping rotational bpm plastic bond

---
+++ src/BPM/bond_bpm_rotational_plastic.h
@@ -0,0 +1,58 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef BOND_CLASS
+// clang-format off
+BondStyle(bpm/rotational/plastic,BondBPMRotationalPlastic);
+// clang-format on
+#else
+
+#ifndef LMP_BOND_BPM_ROTATIONAL_PLASTIC_H
+#define LMP_BOND_BPM_ROTATIONAL_PLASTIC_H
+
+#include "bond_bpm.h"
+
+namespace LAMMPS_NS {
+
+class BondBPMRotationalPlastic : public BondBPM {
+ public:
+  BondBPMRotationalPlastic(class LAMMPS *);
+  ~BondBPMRotationalPlastic() override;
+  void compute(int, int) override;
+  void coeff(int, char **) override;
+  void init_style() override;
+  void settings(int, char **) override;
+  void write_restart(FILE *) override;
+  void read_restart(FILE *) override;
+  void write_restart_settings(FILE *) override;
+  void read_restart_settings(FILE *) override;
+  double single(int, double, int, int, double &) override;
+
+ protected:
+  double *Kr, *Ks, *Kt, *Kb, *gnorm, *gslide, *groll, *gtwist;
+  double *e_plastic, *e_break, *angle_plastic, *angle_break;
+  int smooth_flag, normalize_flag;
+
+  double elastic_plastic_forces(int, int, int, double, double, double, double *, double *, double *,
+                        double *, double *, double *, double &, double &, double &, double &);
+  void damping_forces(int, int, int, double *, double *, double *, double *, double *);
+
+  void allocate();
+  void store_data();
+  double store_bond(int, int, int);
+};
+
+}    // namespace LAMMPS_NS
+
+#endif
+#endif
